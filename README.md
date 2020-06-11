7f0b9164384dc5b757d65ac45fcb52796a7f40b4
bm7 <bayangp0@gmail.com>
2020-06-11 13:00:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+### How does human face mask detector work?
+
+
+### Install dependencies
+ - [x] tensorflow == 
+ - [x] numpy      == 
+ - [x] sklearn    ==
+...
+ - [x] matplotlib ==
+
+### Clone the project
+```
+$git clone https://github.com/bm777/humanface-mask-detector.git
+```
+
+### How to implement your own face mask detector
+ - Training
+
+ - Testing and evaluate
+
+
---
+++ fm_train.py
@@ -0,0 +1,17 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.layers import Dropout, Flatten, Dense, Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from imutils import paths
+
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse, os
+
